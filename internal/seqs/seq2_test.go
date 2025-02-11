b69235a6084f02ac92dbfdc57025d0ba00aae14e
kyuff <soren@sxl.dk>
2025-02-11 11:43:00 +0100

    Add tests for seqs package

--- internal/seqs/seq2_test.go
+++ internal/seqs/seq2_test.go
@@ -8,39 +8,33 @@ import (
 )
 
 func TestSeq2(t *testing.T) {
+	type testCase struct {
+		name  string
+		items []int
+	}
+	tests := []testCase{
+		{
+			name:  "return the full list",
+			items: []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34},
+		},
+		{
+			name:  "return empty list",
+			items: []int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// arrange
+			var got []int
 
-	t.Run("return the full list", func(t *testing.T) {
-		// arrange
-		var (
-			expected = []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34}
-			got      []int
-		)
+			// act
+			for n, err := range seqs.Seq2(tt.items...) {
+				assert.NoError(t, err)
+				got = append(got, n)
+			}
 
-		// act
-		for n, err := range seqs.Seq2(expected...) {
-			assert.NoError(t, err)
-			got = append(got, n)
-		}
-
-		// assert
-		assert.EqualSlice(t, expected, got)
-	})
-
-	t.Run("return empty list", func(t *testing.T) {
-		// arrange
-		var (
-			expected = []int{}
-			got      []int
-		)
-
-		// act
-		for n, err := range seqs.Seq2(expected...) {
-			assert.NoError(t, err)
-			got = append(got, n)
-		}
-
-		// assert
-		assert.EqualSlice(t, expected, got)
-	})
-
+			// assert
+			assert.EqualSlice(t, tt.items, got)
+		})
+	}
 }
