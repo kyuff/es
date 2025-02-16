f8833a5e8832d7458fa97742d0afe3a580c12550
kyuff <soren@sxl.dk>
2025-02-16 16:13:41 +0100

    Test codecs/JSON

--- codecs/json_test.go
+++ codecs/json_test.go
@@ -1,12 +1,13 @@
 package codecs_test
 
 import (
+	"math/rand/v2"
+	"testing"
+
 	"github.com/kyuff/es"
 	"github.com/kyuff/es/codecs"
 	"github.com/kyuff/es/internal/assert"
 	"github.com/kyuff/es/internal/uuid"
-	"math/rand/v2"
-	"testing"
 )
 
 type EventMock struct {
@@ -18,6 +19,34 @@ func (EventMock) Name() string {
 }
 
 func TestJSON(t *testing.T) {
+	t.Run("return error on unknown type", func(t *testing.T) {
+		// arrange
+		var (
+			sut = codecs.NewJSON()
+		)
+
+		// act
+		_, err := sut.Decode("unknown", "unknown", []byte(`{}`))
+
+		// assert
+		assert.Error(t, err)
+	})
+
+	t.Run("return error on malformed json", func(t *testing.T) {
+		// arrange
+		var (
+			sut        = codecs.NewJSON()
+			entityType = uuid.V7()
+			in         = EventMock{ID: rand.Int()}
+		)
+
+		// act
+		_, err := sut.Decode(entityType, "EventMock", []byte(`{ ... not json`))
+
+		// assert
+		assert.Error(t, err)
+	})
+
 	t.Run("should encode and decode", func(t *testing.T) {
 		// arrange
 		var (
