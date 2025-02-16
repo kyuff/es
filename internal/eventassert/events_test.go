d27582390fb15bd02ab62f29d4654bcbf06acaad
kyuff <soren@sxl.dk>
2025-02-16 21:29:29 +0100

    Test internal/eventassert

---
+++ internal/eventassert/events_test.go
@@ -0,0 +1,51 @@
+package eventassert_test
+
+import (
+	"testing"
+
+	"github.com/kyuff/es"
+	"github.com/kyuff/es/internal/assert"
+	"github.com/kyuff/es/internal/eventassert"
+)
+
+func TestEqualEvent(t *testing.T) {
+	t.Run("EqualEvent succeed", func(t *testing.T) {
+		// arrange
+		var (
+			x        = &testing.T{}
+			expected = es.Event{
+				EventNumber: 42,
+			}
+			actual = es.Event{
+				EventNumber: 42,
+			}
+		)
+		// act
+		got := eventassert.EqualEvent(x, expected, actual)
+
+		// assert
+		assert.Truef(t, got, "got")
+		assert.Truef(t, !x.Failed(), "status")
+	})
+
+	t.Run("EqualEvent failed", func(t *testing.T) {
+		// arrange
+		var (
+			x        = &testing.T{}
+			expected = es.Event{
+				EntityID:    "an entity id",
+				EventNumber: 42,
+			}
+			actual = es.Event{
+				EntityID:    "different id",
+				EventNumber: 42,
+			}
+		)
+		// act
+		got := eventassert.EqualEvent(x, expected, actual)
+
+		// assert
+		assert.Truef(t, !got, "got")
+		assert.Truef(t, x.Failed(), "status")
+	})
+}
